914747e8ee96ae4988ad35963cbecd7a1377e585
Anderson Mendes <anderson.mends@gmail.com>
2023-04-14 21:56:24 -0300

    code ready for extract data from telegram

--- ChannelMessages.py
+++ ChannelMessages.py
@@ -2,13 +2,14 @@ import configparser
 import json
 import asyncio
 from datetime import datetime, timedelta
+import re
 
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.types import PeerChannel
 
-# extrai as mensagens do grupo do telegram a partir da data X
+# extract messages from telegram channel from date X
 
 # some functions to parse json date
 class DateTimeEncoder(json.JSONEncoder):
@@ -60,7 +61,9 @@ async def main(phone):
 
     my_channel = await client.get_entity(entity)
 
-    start_date = datetime(2023, 3, 25, 0, 0)  # replace with your desired date
+    # replace with your desired date, for all messages set data before telegram channel
+    start_date = datetime(2023, 3, 25, 0, 0)  
+    
 
     offset_id = 0
     limit = 100
@@ -88,19 +91,55 @@ async def main(phone):
         # Filtering messages to get only those that match the date criteria
         for message in messages:
             if message.date.date() >= start_date.date():
-                message_dict = {'data': message.date.isoformat(), 'message': message.message}
+                message.message = re.sub(r"de .*? até ", "", str(message.message))
+                message.message = re.findall(r"(HM|Max|Gol|Azul|Latam|[0-9]+,[0-9]+)", message.message)
+                message_dict = {'date': message.date.isoformat(), 'message': message.message}
                 all_messages.append(message_dict)
             else:
                 break  # We reached the end of the messages that match the criteria, so we can stop the loop
         
+        # cleaning data removing unecessary data
+        clean_data = []
+        for item in all_messages:
+            new_item = {"date": item["date"]}
+            message = item["message"]
+            i = 0
+            while i < len(message):
+                name = message[i]
+                i += 1
+                if name in ["HM", "Max"]:
+                    new_item["name"] = name
+                elif name in ["Latam", "Gol", "Azul"]:
+                    data_array = []
+                    while i < len(message) and not message[i] in ["Latam", "Gol", "Azul", "HM", "Max"]:
+                        data_array.append(float(message[i].replace(",", ".")))
+                        i += 1
+                    new_item[name] = data_array
+                else:
+                    i += 1
+            clean_data.append(new_item)
+
+        # structuring data to business requeriment
+        ready_data = []
+        for item in clean_data:
+            new_item = {"date": item["date"], "name": item.get("name")}
+            for company in ["Latam", "Gol", "Azul"]:
+                values = item.get(company, [])
+                if values:
+                    max_value = max(values)
+                    new_item[company] = max_value
+            ready_data.append(new_item)
+
         offset_id = messages[-1].id
-        total_messages = len(all_messages)
+        total_messages = len(ready_data)
         
         if total_count_limit != 0 and total_messages >= total_count_limit:
             break
+        
+    # ready_data.reverse() ##to reverse data extract order
 
     with open('channel-messages.json', 'w') as outfile:
-        json.dump(all_messages, outfile, cls=DateTimeEncoder)
+        json.dump(ready_data, outfile, cls=DateTimeEncoder)
 
 with client:
     client.loop.run_until_complete(main(phone))
